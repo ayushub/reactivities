e0d8eaa3cf159b4d48c6f5a543f0f392029971c7
Ayush Agrawal <ayushagrawal@mail.com>
2023-02-14 09:22:15 +1100

    using mobx to manage state of activities

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -7,43 +7,18 @@ import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
 import LoadingComponents from './LoadingComponents';
 import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
   const {activityStore} = useStore()
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then((response) => {
-      let activities:Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split("T")[0]
-        activities.push(activity)
-      })
-      setActivities(activities);
-      setLoading(false);
-    })
-  },[]);
-
-  function handleSelectActivity(id: string){
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  function handleCancelSelectActivity(){
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose(){
-    setEditMode(false);
-  }
+    activityStore.loadActivities()
+  },[activityStore]);
 
   function handleCreateOrEditActivity(activity: Activity){
     setSubmitting(true);
@@ -74,20 +49,13 @@ function App() {
     })
   }
 
-  if (loading) return <LoadingComponents />
+  if (activityStore.loadingInitial) return <LoadingComponents />
   return (
     <>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar/>
       <Container style={{marginTop: "7em"}}>
-        <h2>{activityStore.title}</h2>
         <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
+          activities={activityStore.activities}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
@@ -97,4 +65,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
